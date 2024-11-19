59a856d5bddf166f0878e917bfb8c4b7c83a5f7f
Wang <38373844+123ao69@users.noreply.github.com>
2024-11-19 23:05:36 +0800

    Create block_constant.py

---
+++ src/block_constant.py
@@ -0,0 +1,65 @@
+from base_classes import ODEblock
+import torch
+from utils import get_rw_adj, gcn_norm_fill_val
+
+
+class ConstantODEblock(ODEblock):
+    def __init__(self, odefunc, regularization_fns, opt, data, device, t=torch.tensor([0, 1])):
+        super(ConstantODEblock, self).__init__(odefunc, regularization_fns, opt, data, device, t)
+
+        self.aug_dim = 1
+        self.odefunc = odefunc(self.aug_dim * opt['hidden_dim'], self.aug_dim * opt['hidden_dim'], opt, data, device)
+        self.reg_odefunc.odefunc.edge_index, self.reg_odefunc.odefunc.edge_weight = self.odefunc.edge_index, self.odefunc.edge_weight
+
+        self.odefunc.edge_index = data.edge_index.to(device)
+        self.odefunc.edge_weight = data.edge_attr.to(device)
+
+        if opt['adjoint']:
+            from torchdiffeq import odeint_adjoint as odeint
+        else:
+            from torchdiffeq import odeint
+
+        self.train_integrator = odeint
+        self.test_integrator = odeint
+        self.set_tol()
+
+    def forward(self, x):
+        t = self.t.type_as(x)
+
+        integrator = self.train_integrator if self.training else self.test_integrator
+
+        reg_states = tuple(torch.zeros(x.size(0)).to(x) for i in range(self.nreg))
+
+        func = self.reg_odefunc if self.training and self.nreg > 0 else self.odefunc
+        state = (x,) + reg_states if self.training and self.nreg > 0 else x
+
+        if self.opt["adjoint"] and self.training:
+            state_dt = integrator(
+                func, state, t,
+                method=self.opt['method'],
+                options=dict(step_size=self.opt['step_size'], max_iters=self.opt['max_iters']),
+                adjoint_method=self.opt['adjoint_method'],
+                adjoint_options=dict(step_size=self.opt['adjoint_step_size'], max_iters=self.opt['max_iters']),
+                atol=self.atol,
+                rtol=self.rtol,
+                adjoint_atol=self.atol_adjoint,
+                adjoint_rtol=self.rtol_adjoint)
+        else:
+            state_dt = integrator(
+                func, state, t,
+                method=self.opt['method'],
+                options=dict(step_size=self.opt['step_size'], max_iters=self.opt['max_iters']),
+                atol=self.atol,
+                rtol=self.rtol)
+
+        if self.training and self.nreg > 0:
+            z = state_dt[0][1]
+            reg_states = tuple(st[1] for st in state_dt[1:])
+            return z, reg_states
+        else:
+            z = state_dt[1]
+            return z
+
+    def __repr__(self):
+        return self.__class__.__name__ + '( Time Interval ' + str(self.t[0].item()) + ' -> ' + str(self.t[1].item()) \
+            + ")"
